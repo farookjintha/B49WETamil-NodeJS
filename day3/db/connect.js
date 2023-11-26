d57792d077eeb254fa5889af7a8be4c729bab1a4
Farook <farook.jintha@ace-alternatives.com>
2023-11-26 12:27:08 +0530

    updated the atlas url

--- day3/db/connect.js
+++ day3/db/connect.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const db = async () => {
   try {
     await mongoose.connect(
-      "mongodb://localhost:27017/myShopDB"
-      // "mongodb+srv://farookjintha:Welcome123@b49wetamil.51bb8l2.mongodb.net/?retryWrites=true&w=majority"
+      // "mongodb://localhost:27017/myShopDB"
+      "mongodb+srv://farookjintha:Welcome123@b49wetamil.51bb8l2.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("Database connection established...");
   } catch (error) {
